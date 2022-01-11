6f37170a45296464d4f62122b5f17b2ab49a19a3
RicardoHung <rhung@dataifx.com>
2022-01-11 14:40:36 -0500

    saving changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,38 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+
+// ----------------------------------------------------
+
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 
+// ----------------------------------------------------
+
+export function createTranslateLoader(http: HttpClient) {
+  return new TranslateHttpLoader(http, './assets/translations/', '.json');
+}
+
+// ----------------------------------------------------
+
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent
-  ],
+  declarations: [AppComponent, HeaderComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: createTranslateLoader,
+        deps: [HttpClient],
+      }
+    }),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/modules/inicio/components/contactame/contactame.component.ts
+++ src/app/modules/inicio/components/contactame/contactame.component.ts
@@ -3,13 +3,27 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-contactame',
   templateUrl: './contactame.component.html',
-  styleUrls: ['./contactame.component.scss']
+  styleUrls: ['./contactame.component.scss'],
 })
 export class ContactameComponent implements OnInit {
+  socialMedia: Array<Object>;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+    this.socialMedia = [
+      {
+        icon: './assets/img/social-media/linkedin.png',
+        url: 'https://www.linkedin.com/in/rjh92',
+      },
+      {
+        icon: './assets/img/social-media/github.png',
+        url: 'https://github.com/RicardoHungJS',
+      }
+    ];
   }
 
+  ngOnInit(): void {}
+
+  redirigir(url: string) {
+    window.open(url, '_blank');
+  }
 }
